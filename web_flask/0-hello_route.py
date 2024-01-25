791a62376ac8285acb7f2ab2d7ecf8b49e06fdb5
vincent-mugendi <vincentmwathi5@gmail.com>
2024-01-25 17:08:37 +0000

    Changes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
+@app.route("/airbnb-onepage", strict_slashes=False)
+def hello_hbnb():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
